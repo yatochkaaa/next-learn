a818a7a866746dc242ae94c97ee925e35d684a77
Yatochka <volodymyr.taranin@gmail.com>
2025-09-13 14:07:37 +0300

    Adding Search and Pagination

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,31 @@
+import CardWrapper from "../../ui/dashboard/cards";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { lusitana } from "../../ui/fonts";
+import { Suspense } from "react";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
+
+export default async function Page() {
+  return (
+    <main className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h1>Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
